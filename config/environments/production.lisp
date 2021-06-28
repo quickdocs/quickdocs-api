0476ab26a2b1f949f1eec2200b7c17af2b45abb2
Eitaro Fukamachi <e.arrows@gmail.com>
2021-06-28 21:47:24 +0900

    Allow to set DB connection config by environment variables.

--- config/environments/production.lisp
+++ config/environments/production.lisp
@@ -1,10 +1,12 @@
 (defpackage #:quickdocs-api/config/environments/production
-  (:use #:cl))
+  (:use #:cl
+        #:utopian/config))
 (in-package #:quickdocs-api/config/environments/production)
 
 `(:databases
   ((:maindb . (:postgres
-               :port 25432
-               :database-name "quickdocs"
-               :username "quickdocs"
-               :password "quickdocs"))))
+               :host ,(or (getenv "DB_HOST") "localhost")
+               :port ,(or (getenv-int "DB_PORT") 25432)
+               :database-name ,(or (getenv "DB_NAME") "quickdocs")
+               :username ,(or (getenv "DB_USERNAME") "quickdocs")
+               :password ,(or (getenv "DB_PASSWORD") "quickdocs")))))
