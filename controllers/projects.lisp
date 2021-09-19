71e5091b7bac40a70829c79e5696c49a7713a24c
Eitaro Fukamachi <e.arrows@gmail.com>
2021-09-20 07:55:49 +0900

    Do AND search with space delimiters.

--- controllers/projects.lisp
+++ controllers/projects.lisp
@@ -52,7 +52,7 @@
            (format nil "\\~A" match)))
     (ppcre:regex-replace-all "([%_|*+?{}\\(\\)\\[\\]'])" string #'match-part :simple-calls t)))
 
-(defun search-projects-by-topic (query dist)
+(defun search-projects-by-topic (queries dist)
   (mapcar (lambda (row)
             (apply #'mito:make-dao-instance (find-class 'release) row))
           (mito:retrieve-by-sql
@@ -61,10 +61,12 @@
               (join :dist_release :on (:= :dist_release.release_id :release.id))
               (right-join :project_topic :on (:= :project_topic.project_name :release.name))
               (left-join :project_download_stats :on (:= :project_download_stats.project_name :release.name))
-              (where (:and (:= :dist_id (mito:object-id dist))
-                           (:similar-to :topic
-                                        (format nil "%[[:<:]]~A%"
-                                                (escape-sql-similar-to-meta-char query)))))
+              (where `(:and (:= :dist_id ,(mito:object-id dist))
+                            ,@(mapcar (lambda (query)
+                                        `(:similar-to :topic
+                                                      ,(format nil "%[[:<:]]~A%"
+                                                               (escape-sql-similar-to-meta-char query))))
+                                      queries)))
               (order-by (:desc :download_count :nulls :last)
                         (:desc :release.dist_version))
               (group-by :release.id :download_count)))))
@@ -80,15 +82,17 @@
     (order-by (:desc :download_count :nulls :last)
               (:desc :release.dist_version))))
 
-(defun search-projects-by-description (query dist)
+(defun search-projects-by-description (queries dist)
   (mito:select-dao 'release
     (join :dist_release :on (:= :dist_release.release_id :release.id))
     (left-join :project_download_stats :on (:= :project_download_stats.project_name :release.name))
     (left-join :system :on (:= :system.name :release.name))
-    (where (:and (:= :dist_id (mito:object-id dist))
-                 (:similar-to (:lower :system.description)
-                              (format nil "%[[:<:]]~A%"
-                                      (escape-sql-similar-to-meta-char (string-downcase query))))))
+    (where `(:and (:= :dist_id ,(mito:object-id dist))
+                  ,@(mapcar (lambda (query)
+                              `(:similar-to (:lower :system.description)
+                                            ,(format nil "%[[:<:]]~A%"
+                                                     (escape-sql-similar-to-meta-char query))))
+                       queries)))
     (order-by (:desc :download_count :nulls :last)
               (:desc :release.dist_version))))
 
@@ -102,23 +106,61 @@
      "database")
     (t (string-downcase q))))
 
+(defun space-char-p (char)
+  (and (member char '(#\Space #\Tab #\Newline #\Return))
+       t))
+
+(defun parse-query (q &key (start 0) (end (length q)))
+  (let ((split-pos
+          (if (char= (aref q start) #\")
+              (position #\" q :start (min (1+ start) end) :end end)
+              (position-if #'space-char-p
+                           q
+                           :start start
+                           :end end))))
+    (if (eql split-pos start)
+        (let ((next-start
+                (position-if-not #'space-char-p q :start start :end end)))
+          (if next-start
+              (parse-query q
+                           :start next-start
+                           :end end)
+              nil))
+        (cons
+          (subseq q
+                  (if (char= (aref q start) #\")
+                      (1+ start)
+                      start)
+                  split-pos)
+          (when (and split-pos
+                     (< split-pos end))
+            (let ((next-split-pos
+                    (position-if-not #'space-char-p
+                                     q
+                                     :start (1+ split-pos)
+                                     :end end)))
+              (when next-split-pos
+                (parse-query q :start next-split-pos :end end))))))))
+
 (defun search (params)
   (let ((q (aget params "q")))
     (when (or (not (stringp q))
               (= 0 (length q)))
       (return-from search (listing params)))
 
-    (let ((query (normalize-query q))
+    (let ((queries (mapcar #'normalize-query (parse-query q)))
           (dist (find-latest-dist)))
       (unless dist
         (throw-code 404))
       (let ((releases
               (delete-duplicates
                 (append
-                  (ensure-list (retrieve-exact-match-project query dist))
-                  (search-projects-by-topic query dist)
-                  (search-projects-by-name query dist)
-                  (search-projects-by-description query dist))
+                  (when (null (rest queries))
+                    (ensure-list (retrieve-exact-match-project (first queries) dist)))
+                  (search-projects-by-topic queries dist)
+                  (when (null (rest queries))
+                    (search-projects-by-name (first queries) dist))
+                  (search-projects-by-description queries dist))
                 :key #'release-name
                 :test #'string=
                 :from-end t)))
